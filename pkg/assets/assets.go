a1d2385feceb654e2a656158ce98638b5e01e9e7
Marin Atanasov Nikolov <dnaeon@gmail.com>
2026-03-11 09:01:44 +0200

    assets: add support for resource checksums

--- pkg/assets/assets.go
+++ pkg/assets/assets.go
@@ -5,21 +5,33 @@
 package assets
 
 import (
+	"bufio"
+	"bytes"
 	"embed"
 	_ "embed"
+	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"path/filepath"
+	"strings"
 )
 
-const (
-	// metaFileNamespace specifies the file, which contains the target
-	// namespace for pack resources.
-	metaFileNamespace = ".NAMESPACE"
+// MetaFile represents a metadata file in a pack.
+type MetaFile = string
 
-	// metaFileDesc specifies the file, which provides a short summary of a
+const (
+	// MetaFileNamespace specifies the file, which contains the target
+	// namespace for pack resources.
+	MetaFileNamespace MetaFile = ".NAMESPACE"
+
+	// MetaFileDesc specifies the file, which provides a short summary of a
 	// pack.
-	metaFileDesc = ".DESC"
+	MetaFileDesc MetaFile = ".DESC"
+
+	// MetaFileSums specifies the file, which provides the checksums of pack
+	// resources.
+	MetaFileSums MetaFile = ".SUMS"
 )
 
 // FS is an [embed.FS], which bundles the builtin packs.
@@ -89,16 +101,26 @@ func New(fileSystem fs.FS) (*Collection, error) {
 		packName := filepath.Base(filepath.Dir(packDir))
 		packVersion := filepath.Base(packDir)
 
-		descPath := filepath.Join(packDir, metaFileDesc)
+		descPath := filepath.Join(packDir, MetaFileDesc)
 		desc, err := fs.ReadFile(fileSystem, descPath)
 		if err != nil {
 			return nil, fmt.Errorf("unable to read description file for pack %s@%s: %w", packName, packVersion, err)
 		}
 
-		namespacePath := filepath.Join(packDir, metaFileNamespace)
+		namespacePath := filepath.Join(packDir, MetaFileNamespace)
 		namespace, err := fs.ReadFile(fileSystem, namespacePath)
 		if err != nil {
 			return nil, fmt.Errorf("unable to read namespace file for pack %s@%s: %w", packName, packVersion, err)
+		}
+
+		sumsPath := filepath.Join(packDir, MetaFileSums)
+		sumsData, err := fs.ReadFile(fileSystem, sumsPath)
+		if err != nil {
+			return nil, fmt.Errorf("unable to read sums file for pack %s@%s: %w", packName, packVersion, err)
+		}
+		sums, err := SumsFromReader(bytes.NewReader(sumsData))
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse sums file for pack %s@%s: %w", packName, packVersion, err)
 		}
 
 		resourcePaths, err := fs.Glob(fileSystem, fmt.Sprintf("%s/*.yaml", packDir))
@@ -106,12 +128,15 @@ func New(fileSystem fs.FS) (*Collection, error) {
 			return nil, fmt.Errorf("unable to list resources for pack %s@%s: %w", packName, packVersion, err)
 		}
 
-		// TODO(dnaeon): verify the sha256sum of the file before including it in the pack
-		// TODO(dnaeon): error out if a resource does not have a sha256sum
 		resources := make([]Resource, 0)
 		for _, resourcePath := range resourcePaths {
+			sha256sum, ok := sums[resourcePath]
+			if !ok {
+				return nil, fmt.Errorf("no checksum found for %s", resourcePath)
+			}
 			resource := Resource{
 				Path:       resourcePath,
+				SHA256:     sha256sum,
 				fileSystem: fileSystem,
 			}
 			resources = append(resources, resource)
@@ -154,10 +179,13 @@ type Pack struct {
 	Resources []Resource
 }
 
-// resource represents a resource from a [Pack].
+// Resource represents a resource from a [Pack].
 type Resource struct {
 	// Path represents the path to the resource.
 	Path string
+
+	// SHA256 represents the SHA256 checksum of the resource.
+	SHA256 string
 
 	// fileSystem is the [fs.FS] which contains the resource.
 	fileSystem fs.FS
@@ -166,4 +194,40 @@ type Resource struct {
 // Read reads the resource and returns its contents.
 func (r *Resource) Read() ([]byte, error) {
 	return fs.ReadFile(r.fileSystem, r.Path)
+}
+
+// SumsFromReader parses the checksums from the given reader and returns it as a
+// map.
+//
+// The structure of the sums is expected to follow the conventions used by the
+// GNU coreutils tools such as sha256sum(1).
+//
+// <sha256> /path/to/file/foo
+// <sha256> /path/to/file/bar
+func SumsFromReader(r io.Reader) (map[string]string, error) {
+	sums := make(map[string]string)
+	scanner := bufio.NewReader(r)
+
+	for {
+		line, err := scanner.ReadString('\n')
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		line = strings.Trim(line, "\n")
+		if line == "" {
+			continue
+		}
+
+		items := strings.Split(line, " ")
+		if len(items) != 2 {
+			return nil, fmt.Errorf("invalid sums file entry %q", line)
+		}
+		sums[items[1]] = items[0]
+	}
+
+	return sums, nil
 }
