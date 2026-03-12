f9993e944c30d061802f26efb8d913fcb9f71b41
Marin Atanasov Nikolov <dnaeon@gmail.com>
2026-03-12 09:57:18 +0200

    scripts: initial commit of the `pack-build` script

---
+++ scripts/pack-build.sh
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+
+set -e
+
+_SCRIPT_NAME="${0##*/}"
+_SCRIPT_DIR=$( dirname "$(readlink -f -- "${0}")" )
+_LIBS_DIR="${_SCRIPT_DIR}"
+
+# Set to true in order to enable debugging
+DEBUG=${DEBUG:-false}
+
+if [[ "${DEBUG}" == "true" ]]; then
+  set -x
+fi
+
+# shellcheck source=/dev/null
+source "${_LIBS_DIR}/pack.lib.sh"
+# shellcheck source=/dev/null
+source "${_LIBS_DIR}/logging.lib.sh"
+
+# Prints the usage info of the script
+function _usage() {
+  cat <<_EOF_
+NAME:
+  ${_SCRIPT_NAME} - A utility for building packs
+
+USAGE:
+  ${_SCRIPT_NAME} help
+  ${_SCRIPT_NAME} [b|build] /path/to/pack/dir
+
+EXAMPLES:
+  Display usage info:
+    $ ${_SCRIPT_NAME} help
+
+  Build a pack from a given directory:
+    $ ${_SCRIPT_NAME} build /path/to/some/pack
+
+  Build a pack from a given pack spec file:
+    $ ${_SCRIPT_NAME} build /path/to/some/pack/PACKAGE
+_EOF_
+}
+
+# Builds a pack from a given path.
+#
+# NOTE: This function is meant to called from within a sub-shell.
+#
+# $1: Path to a pack
+function _build_pack() {
+  local _pack_path="${1}"
+  local _pack_spec_base_dir=""
+
+  # Sanity checks
+  if [[ -z "${_pack_path}" ]]; then
+    _msg_error "_build_pack: no pack dir has been specified" 1
+  fi
+
+  _pack_spec_base_dir="$( _get_pack_spec_base_dir "${_pack_path}" )"
+  if [[ -z "${_pack_spec_base_dir}" ]]; then
+    _msg_error "_build_pack: unable to find base pack dir for ${_pack_path}" 1
+  fi
+
+  # Pack build steps
+  set -e
+
+  # shellcheck source=/dev/null
+  source "${_LIBS_DIR}/tools.lib.sh"
+
+  # Source and sanity check the pack spec
+  local _pack_spec_file="${_pack_spec_base_dir}/${PACK_SPEC_FILE}"
+  # shellcheck source=/dev/null
+  source "${_pack_spec_file}"
+
+  # Require the presence of the following vars in the pack spec
+  local _want_vars=(
+    NAME
+    VERSION
+    DESCRIPTION
+    NAMESPACE
+  )
+
+  for _var in "${_want_vars[@]}"; do
+    if [ -z "${!_var}" ]; then
+      _msg_error "_build_pack: required var ${_var} is not set in pack spec @ ${_pack_spec_file}" 1
+    fi
+  done
+
+  # Each pack spec must define a `package()' function
+  type -t package >& /dev/null || {
+    _msg_error "_build_pack: ${_pack_spec_file} does not provide a package() function" 1
+  }
+
+  _msg_info "Setting up environment for pack ${NAME}@${VERSION} ..."
+  SRC_DIR="${_pack_spec_base_dir}"  # Source directory points to the pack spec dir
+  PACK_DIR="${ASSETS_PKG}/packs/${NAME}/${VERSION}"
+  mkdir -p "${PACK_DIR}"
+  PACK_DIR=$( realpath "${PACK_DIR}" )
+
+  # Make SRC_DIR and PACK_DIR available to package() functions
+  export SRC_DIR PACK_DIR
+
+  # Package it up
+  _msg_info "Building pack ${NAME}@${VERSION} ..."
+  package
+
+  # Run tests, if a `package_test()' function has been provided by the spec
+  type -t package_test >& /dev/null && {
+    _msg_info "Running tests for pack ${NAME}@${VERSION} ..."
+    package_test
+  }
+
+  # Generate metadata files for the pack
+  _msg_info "Generating metadata files for pack ${NAME}@${VERSION} ..."
+  echo "${NAMESPACE}" > "${PACK_DIR}/${PACK_METADATA_NAMESPACE}"
+  echo "${DESCRIPTION}" > "${PACK_DIR}/${PACK_METADATA_DESC}"
+
+  # Re-create sums file
+  if [[ -f "${PACK_DIR}/${PACK_METADATA_SUMS}" ]]; then
+    rm -f "${PACK_DIR}/${PACK_METADATA_SUMS}"
+  fi
+
+  cd "${ASSETS_PKG}"
+  find "packs/${NAME}/${VERSION}" \
+       -iname "${PACK_RESOURCES_GLOB}" \
+       -exec sha256sum {} \; | tee -a "${PACK_DIR}/${PACK_METADATA_SUMS}"
+  cd "${OLDPWD}"
+  _msg_info "Pack ${NAME}@${VERSION} built successfully"
+}
+
+# Main entrypoint
+function _main() {
+  if [[ $# -ne 2 ]]; then
+    _usage
+    exit 64  # EX_USAGE
+  fi
+
+  local _operation="${1}"
+  local _pack_path="${2}"
+
+  case "${_operation}" in
+    h|help)
+      _usage
+      exit 0
+      ;;
+    b|build)
+      # NOTE: pack building should be executed from within a sub-shell
+      (
+        _build_pack "${_pack_path}"
+      )
+      ;;
+    *)
+      _msg_error "unknown command ${_operation}" 64  # EX_USAGE
+      ;;
+  esac
+}
+
+_main "$@"
