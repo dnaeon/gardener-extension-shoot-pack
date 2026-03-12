6356034339b13be036d4bf12fa6de1436432697b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2026-03-12 08:47:33 +0200

    scripts: add a lib with common pack-related functions and vars

---
+++ scripts/pack.lib.sh
@@ -0,0 +1,40 @@
+# -*- mode: bash-ts-mode; sh-basic-offset 2; -*-
+
+_LIB_NAME="${0##*/}"
+_LIB_DIR=$( dirname "$(readlink -f -- "${0}")" )
+_PROJECT_ROOT="$( dirname ${_LIB_DIR} )"
+
+# shellcheck source=/dev/null
+source "${_LIBS_DIR}/logging.lib.sh"
+
+# Export common vars for use by clients
+export PACK_METADATA_DESC=".DESC"
+export PACK_METADATA_NAMESPACE=".NAMESPACE"
+export PACK_METADATA_SUMS=".SUMS"
+export PACK_SPEC_FILE="PACKAGE"
+export PACK_RESOURCES_GLOB="*.yaml"
+export ASSETS_PKG="${_PROJECT_ROOT}/pkg/assets"
+
+# Returns the base dir of a pack
+#
+# $1: Path to a pack file or directory
+function _get_pack_base_dir() {
+  local _path="${1}"
+  local _base_dir=""
+
+  if [[ -z "${_path}" ]]; then
+    _msg_error "_get_pack_base_dir: empty pack dir specified" 1
+  fi
+
+  if [[ "$( basename ${_path} )" == "${PACK_SPEC_FILE}" ]]; then
+    # We were called with a path to a pack file
+    _base_dir="$( dirname ${_path} )"
+  elif [[ -f "${_path}/${PACK_SPEC_FILE}" ]]; then
+    # We were called with a path, which contains a pack file
+    _base_dir="${_path}/${_PACK_SPEC_FILE}"
+  else
+    _msg_error "_get_pack_base_dir: unable to find a pack spec at ${_path}" 1
+  fi
+
+  echo "$( dirname ${_base_dir} )"
+}
