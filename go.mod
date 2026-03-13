b4ffe9143412e3ee4241adc6e5ef1c7394db18c5
Marin Atanasov Nikolov <dnaeon@gmail.com>
2026-03-13 09:30:44 +0200

    actuator: implement management of ManagedResources for packs

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	k8s.io/component-base v0.35.2
 	k8s.io/utils v0.0.0-20260210185600-b8788abfbbc2
 	sigs.k8s.io/controller-runtime v0.23.3
+	sigs.k8s.io/kustomize/api v0.21.1
 )
 
 require (
@@ -74,6 +75,7 @@ require (
 	github.com/gardener/cert-management v0.19.0 // indirect
 	github.com/gardener/etcd-druid/api v0.35.1 // indirect
 	github.com/gardener/machine-controller-manager v0.61.2 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-jose/go-jose/v4 v4.1.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
@@ -123,6 +125,7 @@ require (
 	github.com/moby/spdystream v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
+	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/muhlemmer/gu v0.3.1 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
@@ -157,6 +160,7 @@ require (
 	github.com/valyala/histogram v1.2.0 // indirect
 	github.com/valyala/quicktemplate v1.8.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	github.com/xlab/treeprint v1.2.0 // indirect
 	github.com/zitadel/oidc/v3 v3.38.1 // indirect
 	github.com/zitadel/schema v1.3.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
@@ -217,6 +221,7 @@ require (
 	k8s.io/pod-security-admission v0.35.1 // indirect
 	sigs.k8s.io/gateway-api v1.3.0 // indirect
 	sigs.k8s.io/json v0.0.0-20250730193827-2d320260d730 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.21.1 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v6 v6.3.2-0.20260122202528-d9cc6641c482 // indirect
 	sigs.k8s.io/yaml v1.6.0 // indirect
--- pkg/actuator/pack/actuator.go
+++ pkg/actuator/pack/actuator.go
@@ -10,20 +10,29 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"path/filepath"
+	"slices"
+	"strconv"
 
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	"github.com/gardener/gardener/extensions/pkg/controller/extension"
 	v1beta1helper "github.com/gardener/gardener/pkg/api/core/v1beta1/helper"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/utils/managedresources"
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/component-base/featuregate"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/kustomize/api/provider"
+	"sigs.k8s.io/kustomize/api/resource"
 
 	"github.com/gardener/gardener-extension-shoot-pack/pkg/apis/config"
 	"github.com/gardener/gardener-extension-shoot-pack/pkg/apis/config/validation"
-	"github.com/gardener/gardener-extension-shoot-pack/pkg/metrics"
+	"github.com/gardener/gardener-extension-shoot-pack/pkg/assets"
 )
 
 // ErrInvalidActuator is an error which is returned when creating an [Actuator]
@@ -38,12 +47,19 @@ const (
 	ExtensionType = "shoot-pack"
 	// FinalizerSuffix is the finalizer suffix used by the actuator
 	FinalizerSuffix = "gardener-extension-shoot-pack"
+	// ManagedResourcePrefix is the prefix of the ManagedResources created
+	// by the extension controller.
+	ManagedResourcePrefix = "extension-shoot-pack"
+	// ManagedResourceOrigin is the origin of the ManagedResources created
+	// by the extension controller.
+	ManagedResourceOrigin = "gardener-extension-shoot-pack"
 )
 
 // Actuator is an implementation of [extension.Actuator].
 type Actuator struct {
-	client  client.Client
-	decoder runtime.Decoder
+	client          client.Client
+	decoder         runtime.Decoder
+	resourceFactory *resource.Factory
 
 	// The following fields are usually derived from the list of extra Helm
 	// values provided by gardenlet during the deployment of the extension.
@@ -69,6 +85,7 @@ func New(c client.Client, opts ...Option) (*Actuator, error) {
 
 	act := &Actuator{
 		client:                c,
+		resourceFactory:       provider.NewDefaultDepProvider().GetResourceFactory(),
 		gardenletFeatureGates: make(map[featuregate.Feature]bool),
 	}
 
@@ -160,11 +177,6 @@ func (a *Actuator) Reconcile(ctx context.Context, logger logr.Logger, ex *extens
 	// [extensionsv1alpha1.Extension] resource.
 	clusterName := ex.Namespace
 
-	// Increment our example metrics counter
-	defer func() {
-		metrics.ActuatorOperationTotal.WithLabelValues(clusterName, "reconcile").Inc()
-	}()
-
 	logger.Info("reconciling extension", "name", ex.Name, "cluster", clusterName)
 
 	cluster, err := extensionscontroller.GetCluster(ctx, a.client, clusterName)
@@ -192,22 +204,141 @@ func (a *Actuator) Reconcile(ctx context.Context, logger logr.Logger, ex *extens
 		return err
 	}
 
-	// TODO(user): implement the main reconciliation logic
+	// Each pack gets its own managed resource
+	collection, err := assets.New(assets.FS, assets.WithSkipVerify(false))
+	if err != nil {
+		return err
+	}
+
+	enabledPacks := make([]config.Pack, 0)
+	disabledPacks := make([]config.Pack, 0)
+	for _, pack := range cfg.Spec.Packs {
+		if err := a.createManagedResourceForPack(ctx, ex.Namespace, collection, pack); err != nil {
+			return err
+		}
+		enabledPacks = append(enabledPacks, pack)
+	}
+
+	// Delete ManagedResources for packs, which are have not been
+	// configured.
+	for _, pack := range collection.Packs {
+		if !slices.ContainsFunc(enabledPacks, func(item config.Pack) bool {
+			return item.Name == pack.Name && item.Version == pack.Version
+		}) {
+			disabledPacks = append(disabledPacks, config.Pack{Name: pack.Name, Version: pack.Version})
+		}
+	}
+
+	for _, disabledPack := range disabledPacks {
+		mrName := fmt.Sprintf("%s-%s", ManagedResourcePrefix, disabledPack.Name)
+		if err := managedresources.DeleteForShoot(ctx, a.client, ex.Namespace, mrName); client.IgnoreNotFound(err) != nil {
+			return err
+		}
+	}
 
 	return nil
+}
+
+// createManagedResourceForPack creates a new ManagedResource with the resources
+// provided by the pack.
+func (a *Actuator) createManagedResourceForPack(ctx context.Context, shootNamespace string, collection *assets.Collection, packConfig config.Pack) error {
+	if !collection.PackExists(packConfig.Name, packConfig.Version) {
+		return fmt.Errorf("pack %s@%s does not exist", packConfig.Name, packConfig.Version)
+	}
+
+	pack, err := collection.GetPack(packConfig.Name, packConfig.Version)
+	if err != nil {
+		return err
+	}
+
+	registry := managedresources.NewRegistry(
+		kubernetes.ShootScheme,
+		kubernetes.ShootCodec,
+		kubernetes.ShootSerializer,
+	)
+
+	// Namespace for the pack resources
+	packNamespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: pack.Namespace,
+		},
+	}
+	if err := registry.Add(packNamespace); err != nil {
+		return fmt.Errorf("unable to register pack namespace resource: %w", err)
+	}
+
+	// Register pack resources with the registry
+	for _, packResource := range pack.Resources {
+		resourceData, err := packResource.Read()
+		if err != nil {
+			return fmt.Errorf("unable to read pack resource %s", packResource.Path)
+		}
+
+		// Create [resource.Resource] items from the pack resource.
+		// Take into account that a single pack resource may contain
+		// multiple Kubernetes resources.
+		resourceItems, err := a.resourceFactory.SliceFromBytes(resourceData)
+		if err != nil {
+			return fmt.Errorf("unable to create resource from %s: %w", packResource.Path, err)
+		}
+
+		// Adjust the namespace for each namespace-scoped resources.
+		for idx, r := range resourceItems {
+			gvk := r.GetGvk()
+			if !gvk.IsClusterScoped() {
+				if err := r.SetNamespace(pack.Namespace); err != nil {
+					return fmt.Errorf("unable to set namespace for %s: %w", packResource.Path, err)
+				}
+			}
+
+			// ... and register
+			data, err := r.AsYAML()
+			if err != nil {
+				return fmt.Errorf("unable to marshal %s: %w", packResource.Path, err)
+			}
+
+			registry.AddSerialized(
+				filepath.Join(packResource.Path, strconv.Itoa(idx)),
+				data,
+			)
+		}
+	}
+
+	data, err := registry.SerializedObjects()
+	if err != nil {
+		return err
+	}
+
+	mrName := fmt.Sprintf("%s-%s", ManagedResourcePrefix, pack.Name)
+
+	return managedresources.CreateForShoot(
+		ctx,
+		a.client,
+		shootNamespace,
+		mrName,
+		ManagedResourceOrigin,
+		false,
+		data,
+	)
 }
 
 // Delete deletes any resources managed by the [Actuator]. This method
 // implements the [extension.Actuator] interface.
 func (a *Actuator) Delete(ctx context.Context, logger logr.Logger, ex *extensionsv1alpha1.Extension) error {
-	// Increment our example metrics counter
-	defer func() {
-		metrics.ActuatorOperationTotal.WithLabelValues(ex.Namespace, "delete").Inc()
-	}()
-
 	logger.Info("deleting resources managed by extension")
 
-	// TODO(user): implement logic for deleting anything managed by the extension
+	// Delete all managed resources for all packs
+	collection, err := assets.New(assets.FS, assets.WithSkipVerify(false))
+	if err != nil {
+		return err
+	}
+
+	for _, pack := range collection.Packs {
+		mrName := fmt.Sprintf("%s-%s", ManagedResourcePrefix, pack.Name)
+		if err := managedresources.DeleteForShoot(ctx, a.client, ex.Namespace, mrName); client.IgnoreNotFound(err) != nil {
+			return err
+		}
+	}
 
 	return nil
 }
@@ -216,26 +347,14 @@ func (a *Actuator) Delete(ctx context.Context, logger logr.Logger, ex *extension
 // because of a force-delete event of the shoot cluster. This method implements
 // the [extension.Actuator] interface.
 func (a *Actuator) ForceDelete(ctx context.Context, logger logr.Logger, ex *extensionsv1alpha1.Extension) error {
-	// Increment our example metrics counter
-	defer func() {
-		metrics.ActuatorOperationTotal.WithLabelValues(ex.Namespace, "force_delete").Inc()
-	}()
-
 	logger.Info("shoot has been force-deleted, deleting resources managed by extension")
 
-	// TODO(user): implement logic for deleting anything managed by the extension
-
-	return nil
+	return a.Delete(ctx, logger, ex)
 }
 
 // Restore restores the resources managed by the extension [Actuator]. This
 // method implements the [extension.Actuator] interface.
 func (a *Actuator) Restore(ctx context.Context, logger logr.Logger, ex *extensionsv1alpha1.Extension) error {
-	// Increment our example metrics counter
-	defer func() {
-		metrics.ActuatorOperationTotal.WithLabelValues(ex.Namespace, "restore").Inc()
-	}()
-
 	return a.Reconcile(ctx, logger, ex)
 }
 
@@ -243,10 +362,5 @@ func (a *Actuator) Restore(ctx context.Context, logger logr.Logger, ex *extensio
 // because of a shoot control-plane migration event. This method implements the
 // [extension.Actuator] interface.
 func (a *Actuator) Migrate(ctx context.Context, logger logr.Logger, ex *extensionsv1alpha1.Extension) error {
-	// Increment our example metrics counter
-	defer func() {
-		metrics.ActuatorOperationTotal.WithLabelValues(ex.Namespace, "migrate").Inc()
-	}()
-
 	return a.Reconcile(ctx, logger, ex)
 }
